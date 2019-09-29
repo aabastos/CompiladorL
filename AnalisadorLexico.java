e5dbae0352dc526497fcbbb01dab8ef4de416925
aabastos <andrebastos1198@gmail.com>
2019-09-29 14:53:15 -0300

    Começo analisador lexico

---
+++ AnalisadorLexico.java
@@ -0,0 +1,31 @@
+public class AnalisadorLexico {
+  private Leitor leitor;
+  private Caracteres verificadorCaracteres;
+  private int estadoInicial = 0;
+  private int estadoFinal = 8;
+
+  public AnalisadorLexico(Leitor leitor) {
+    this.leitor = leitor;
+    this.verificadorCaracteres = new Caracteres();
+  }
+
+  public Registro getProximoRegistro() {
+    String lexema = "";
+    Tipo tipoConstante = null;
+    int estado = estadoInicial;
+
+    while (estado != estadoFinal) {
+      char proximo = leitor.getProximoCaractere();
+
+      switch (estado) {
+      case 0:
+        if (verificadorCaracteres.letra(proximo)) {
+          estado = 3;
+          lexema = lexema + proximo;
+        }
+
+        break;
+      }
+    }
+  }
+}
--- Caracteres.java
+++ Caracteres.java
@@ -2,11 +2,8 @@ public class Caracteres {
   public static char letras[] = { 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i',
       'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
       'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z' };
-
   public static char digitos[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
   public static char simbolos[] = { '<', '>', '=', '!', '+', '-', '/', '*', '(', ')', '\'', ';', ':' };
-
   public static char brancos[] = { 10, 13, '\n', ' ' };
 
   public boolean letra(char ch) {
